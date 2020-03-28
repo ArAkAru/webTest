c85325d5b9acad11aa2815f014940f0374ce35f4
Arkadiy <arkadi97@mail.ru>
2020-03-28 16:50:38 +0300

    add email sender

--- src/main/java/weptest/Handler.java
+++ src/main/java/weptest/Handler.java
@@ -1,53 +1,62 @@
 package weptest;
-
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RestController;
 import Entity.Message;
 import Service.MessageService;
-import Service.MessageServiceImpl;
-import repMes.repoMes;
+
+
 
 @Controller
 public class Handler {
 	@Autowired
 	MessageService messageService;
-	
+	@Autowired
+	MailSender mailSender;
 	public Handler() {
 		
 	}
-	@RequestMapping
-	public String showPage(HttpServletRequest req,Model model)
+//	@RequestMapping
+//	public String showPage(HttpServletRequest req,Model model)
+//	{
+//		
+//		return "main-menu";    
+//    
+//	}
+//	@PostMapping
+//    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+//		
+//		
+//		Message mes= new Message(text,tag);
+//		messageService.addMes(mes);
+//		
+//		
+//		
+//
+//        return "main-menu";
+//    }
+	@RequestMapping("mail-form")
+	public String showPage()
 	{
-		return "main-menu";    
+		
+		return "mail";    
     
 	}
-	@PostMapping
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-		
-		
-		Message mes= new Message(text,tag);
-		messageService.addMes(mes);
-		
-		
-		
-
-        return "main-menu";
-    }
+	@PostMapping("mail-form")
+	public String sendMail(@RequestParam String mail) throws MessagingException {
+		System.out.println(mail);
+		mailSender.send(mail, "Test mail from ArAkAru", "Howdy");
+		return "mail";
+			
+	}
 
 }
---
+++ src/main/webapp/WEB-INF/view/mail.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE>
+<html>
+
+<body>
+    
+        <form method="post" >
+            <input type="text" name="mail" placeholder="Write" />
+            <button type="submit">Add</button>
+        </form>
+    
+    
+    
+</body>
+
+</html>
